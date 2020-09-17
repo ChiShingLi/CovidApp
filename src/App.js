91dc2c1997b3ada03c107cc0ce8c913e27d53026
cli16 <cli16@mail.wou.edu>
2020-09-17 15:17:43 -0700

    implemented API data fetching feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react"
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loadingData: true,
+      apiData: []
+    }
+  }
+
+  //fetch the API data, when page loads
+  componentDidMount() {
+
+    fetch("https://api.covid19api.com/summary")
+      .then(res => res.json()) //convert to JSON
+      .then(response => {
+        this.setState({
+          //store the data in the current
+          apiData: response,
+          loadingData: false
+        })
+        console.log(this.state.apiData)
+        console.log(this.state.loadingData)
+      })
+  }
+
+
   render() {
     return (
       <div>
-        init
+        {this.state.loadingData ? <h1>Fetching Data</h1> : <h1>done loading</h1>}
       </div>
     )
   }
